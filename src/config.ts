d850a5e7b9c8c009233ba8529adea01775c2ef4b
steinhakasevs <stein.hakase.vs@gmail.com>
2020-08-09 00:42:05 -0600

    changed the struccture of config for easier login

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,10 @@
 import 'dotenv/config'
 
-export default {
+const config = {
   environment: process.env.NODE_ENV || 'development',
   port: process.env.PORT || '4000',
   apikey: process.env.API_KEY || null,
   allowedOrigins: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS?.split(',') : '*'
 }
+
+export default config
