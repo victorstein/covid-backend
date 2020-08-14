516eb4f304dc991786599c004c3137ea78d35ae5
steinhakasevs <stein.hakase.vs@gmail.com>
2020-08-13 18:47:06 -0600

    removed valid country validation

--- src/resolvers/covid/interfaces/countryStatisticsInterface.ts
+++ src/resolvers/covid/interfaces/countryStatisticsInterface.ts
@@ -1,12 +1,12 @@
 import { ArgsType, Field } from "type-graphql";
 import { IsString } from "class-validator";
-import isValidCountry from '../../../utils/customDecorators/isValidCountry'
+// import isValidCountry from '../../../utils/customDecorators/isValidCountry'
 
 @ArgsType()
 
 export default class CountryStatisticsInterface {
   @Field({ nullable: true })
   @IsString()
-  @isValidCountry({ message: 'The provided country is invalid' })
+  // @isValidCountry({ message: 'The provided country is invalid' })
   name: string
 }
